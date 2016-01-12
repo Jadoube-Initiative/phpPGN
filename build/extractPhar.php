9587a76ef48a7c89af20b2b5a7c12e0a707756dc
Geraldo B. Landre <geraldo.landre@gmail.com>
2016-01-12 16:20:14 -0300

    Reverting to c9070a8

--- build/extractPhar.php
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env php
-<?php
-$phar = new Phar($argv[1]);
-$phar->extractTo($argv[2]);
--- src/stub.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * Copyright (c) 2016 Geraldo B. Landre
- *
- * See the file LICENSE for copying permission.
- */
-
-include_once 'bootstrap.php';
